65a94dbe243c7aa45cdcdb38291ad9bd9e07a854
Lucas Martins <lucaslealm@gmail.com>
2019-02-26 16:55:54 -0300

    Model and controller created / db seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles/list'
+  get 'articles/show'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+
 end
