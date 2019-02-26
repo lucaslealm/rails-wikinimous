2a598acefad6b45b5940f9b61fff37198e635599
Lucas Martins <lucaslealm@gmail.com>
2019-02-26 17:23:23 -0300

    E2 partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,20 @@
 class ArticlesController < ApplicationController
-  def list
+  def index
+    @articles = Article.all
   end
 
   def show
   end
 
+  def new
+
+  end
+
   def create
+  end
+
+  def edit
+
   end
 
   def update
